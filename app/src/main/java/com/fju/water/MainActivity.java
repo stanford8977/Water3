6cb3e35ce7cf64a50ada91e29e13aeb1a1d06368
stanford8977 <stanford8977@gmail.com>
2019-12-03 16:18:15 +0800

    arrays

--- app/src/main/java/com/fju/water/MainActivity.java
+++ app/src/main/java/com/fju/water/MainActivity.java
@@ -12,17 +12,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG = MainActivity.class.getSimpleName();
     private TextView month;
         private Button result;
     private double outcome;
@@ -30,9 +35,12 @@ public class MainActivity extends AppCompatActivity {
     private Double outcome1;
     boolean isNext =false;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate" );
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         month = findViewById(R.id.month);
@@ -78,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                                 .setMessage(getString(R.string.fee)+outcome)
                                 .setPositiveButton(getString(R.string.ok),null)
                                 .show();
+
                        /* Intent intent = new Intent(MainActivity.this, ResultActivity.class);
                         intent.putExtra(getString(R.string.extra_fee), outcome);
                         startActivity(intent);*/
@@ -118,6 +127,54 @@ public class MainActivity extends AppCompatActivity {
                 }*/
             }
         });
+        Spinner cities = findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG , getResources().getStringArray(R.array.cities)[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart" );
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume" );
     }
 
     @Override
